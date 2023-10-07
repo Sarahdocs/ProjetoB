fca046d2a726ae5bb528427012dae8a02ac1cd75
Sarahdocs <avilaarbao@gmail.com>
2023-10-07 14:22:34 -0300

    Atualizações referente aos empréstimos

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -18,3 +18,44 @@
     }
 }
 onblur="validaCampo()"*/
+
+function validar(){
+   var Titulo = form.Titulo.value;
+   var Autor = form.Autor.value;
+   var Ano = form.Ano.value;
+
+   if(Titulo == ""){
+    alert('Preencha o campo Titulo!');
+    form.Titulo.focus();
+    return false
+   }
+
+   if(Autor == ""){
+    alert('Preencha o campo Autor!');
+    form.Autor.focus();
+    return false
+   }
+
+   if(Ano == ""){
+    alert('Preencha o campo Ano!');
+    form.Ano.focus();
+    return false
+   }
+}
+
+function validarCampo(){
+    var Nome = form2.Nome.value;
+    var Telefone = form2.Telefone.value;
+    
+    if(Nome == ""){
+        alert('Preencha o campo Nome!');
+        form2.Nome.focus();
+        return false
+    }
+
+    if(Telefone == ""){
+        alert('Preencha o campo Telefone!');
+        form2.Telefone.focus();
+        return false
+    }
+}
