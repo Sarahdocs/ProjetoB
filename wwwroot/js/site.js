b692049dd55d2c253999f9d1c5612b416f6f5cd0
Sarahdocs <avilaarbao@gmail.com>
2023-10-07 12:15:58 -0300

    Atualizações dos erros vinculados a lista de livros

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,19 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+/*function validaCampo()
+{
+    var campoUsuario = document.getElementById('submit'); 
+
+    if(campoUsuario.value.length <1)
+    {
+        alert("Verifique se todos os campos estão preenchidos.");
+        return false;
+    }
+    else
+    {
+       return true;
+    }
+}
+onblur="validaCampo()"*/
